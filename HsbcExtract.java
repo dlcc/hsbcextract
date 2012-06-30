195d1144b7992228e9aa431790e8d6771cb384f4
Ian Murray <Ian@Home>
2012-07-01 00:42:05 +0100

    Added code to output the correct year when statement spanned the new year

--- HsbcExtract.java
+++ HsbcExtract.java
@@ -11,17 +11,17 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
     <http://www.gnu.org/licenses/>.
-    
+
     Usage:
-    
+
     Access "your statement" of hsbc's UK Personal banking website and right-click on the
     links to your individual statements. Then use "save link as..." and save them all to
     a single folder. Then use :-
-    
+
     java -jar hsbcextract.jar <FileOrFoldername> > hsbc.csv
-    
+
     ... to create a CSV file of your entire statements for analysis.
-    
+
  */
 
 package org.ianmurray.hsbcextract;
@@ -62,7 +62,56 @@ public class HsbcExtract {
 			doFile(inp);
 		}
 	}
-	
+	private static String findYear(String statementDateStr, String entryDateStr)
+	{
+		// This is a nonsense method to set the right year for an entry when it occurs in
+		// a previous year to the statement date.
+		// Too much effort for the HSBC to include the year in the entry date, it would seem.
+
+		int statementMonth;
+		int entryMonth;
+		int year;
+
+		statementMonth = 0;
+		if (statementDateStr.matches(".*Jan.*")) statementMonth = 1;
+		if (statementDateStr.matches(".*Feb.*")) statementMonth = 2;
+		if (statementDateStr.matches(".*Mar.*")) statementMonth = 3;
+		if (statementDateStr.matches(".*Apr.*")) statementMonth = 4;
+		if (statementDateStr.matches(".*May.*")) statementMonth = 5;
+		if (statementDateStr.matches(".*Jun.*")) statementMonth = 6;
+		if (statementDateStr.matches(".*Jul.*")) statementMonth = 7;
+		if (statementDateStr.matches(".*Aug.*")) statementMonth = 8;
+		if (statementDateStr.matches(".*Sep.*")) statementMonth = 9;
+		if (statementDateStr.matches(".*Oct.*")) statementMonth = 10;
+		if (statementDateStr.matches(".*Nov.*")) statementMonth = 11;
+		if (statementDateStr.matches(".*Dec.*")) statementMonth = 12;
+
+		entryMonth=0;
+		if (entryDateStr.matches(".*Jan.*")) entryMonth = 1;
+		if (entryDateStr.matches(".*Feb.*")) entryMonth = 2;
+		if (entryDateStr.matches(".*Mar.*")) entryMonth = 3;
+		if (entryDateStr.matches(".*Apr.*")) entryMonth = 4;
+		if (entryDateStr.matches(".*May.*")) entryMonth = 5;
+		if (entryDateStr.matches(".*Jun.*")) entryMonth = 6;
+		if (entryDateStr.matches(".*Jul.*")) entryMonth = 7;
+		if (entryDateStr.matches(".*Aug.*")) entryMonth = 8;
+		if (entryDateStr.matches(".*Sep.*")) entryMonth = 9;
+		if (entryDateStr.matches(".*Oct.*")) entryMonth = 10;
+		if (entryDateStr.matches(".*Nov.*")) entryMonth = 11;
+		if (entryDateStr.matches(".*Dec.*")) entryMonth = 12;
+
+
+		String yearString =	statementDateStr.substring(statementDateStr.length()-4, statementDateStr.length());
+		year = Integer.parseInt(yearString);
+		if (statementMonth < entryMonth)
+		{
+			year--;
+		}
+
+
+		return entryDateStr.substring(0,2).concat("/").concat(Integer.toString(entryMonth).concat("/").concat(Integer.toString(year)));
+
+	}
 	private static void doFile(File input)
 	{
 
@@ -76,28 +125,35 @@ public class HsbcExtract {
 			Element statementDate = doc.select("div:contains(Statement date) + div").first();
 
 			table = doc.select("tbody:eq(1)").first();
-			allData = table.select("td");
-			Iterator<Element> itr = allData.iterator();
-			Element itrElement;
-			int kounter = 0;
-			String statementDateString = statementDate.text().trim();
-			statementDateString = statementDateString.substring(statementDateString.length()-4);
-			while(itr.hasNext()) {
-
-				itrElement =itr.next();
-				String truncated = itrElement.text().replace((char)160,(char)32).trim();
-				System.out.print(truncated);
-				if (kounter == 0)
+			if (table != null)
+			{
+				allData = table.select("td");
+				if (allData != null)
 				{
+					Iterator<Element> itr = allData.iterator();
+					Element itrElement;
+					int kounter = 0;
+					String statementDateString = statementDate.text().trim();
+					while(itr.hasNext()) {
 
-					System.out.print(" ".concat(statementDateString));
-				}
-				System.out.print(",");
-				kounter++;
-				if (kounter == 7)
-				{
-					kounter = 0;
-					System.out.println("");
+						itrElement =itr.next();
+						String truncated = itrElement.text().replace((char)160,(char)32).trim();
+
+						if (kounter == 0)
+						{
+
+							System.out.print(findYear(statementDateString,truncated));
+						} else {
+							System.out.print(truncated);
+						}
+						System.out.print("|");
+						kounter++;
+						if (kounter == 7)
+						{
+							kounter = 0;
+							System.out.println("");
+						}
+					}
 				}
 			}
 		} catch (IOException ioe) {
